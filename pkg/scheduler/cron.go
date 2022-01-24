2545e158a88be647a60c411eb078ac63628d24b8
shepard <eviltomorrow@163.com>
2022-01-24 20:08:27 +0800

    dev

--- pkg/scheduler/cron.go
+++ pkg/scheduler/cron.go
@@ -20,58 +20,57 @@ var (
 
 func Run() {
 	var c = cron.New()
-	_, err := c.AddFunc(DefaultCronSpec, func() {
-		var (
-			now = time.Now()
-			ok  = false
-		)
-		defer func() {
-			if !ok {
-				client, close, err := client.NewClientForNotification()
-				if err != nil {
-					zlog.Error("NewClientForNotification failure", zap.Error(err))
-					return
-				}
-				defer close()
-
-				ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-				defer cancel()
-
-				if _, err := client.SendEmail(ctx, &pb.Mail{
-					To: []*pb.Contact{
-						{Name: "shepard", Address: "eviltomorrow@163.com"},
-					},
-					Subject: "[Warnning] Robber-collector panic notify",
-					Body:    fmt.Sprintf("<h4>Fetch metadata failure, please check in log, date: %v</h4>", time.Now().Format("2006-01-02 15:04:05")),
-				}); err != nil {
-					zlog.Error("SendEmail failure", zap.Error(err))
-				}
-			}
-		}()
-
-		zlog.Info("Fetch metadata begin", zap.String("date", now.Format("2006-01-02")))
-		date, fetchCount, err := service.CollectMetadataToMongoDB(now, Source, true)
-		if err != nil {
-			zlog.Error("CollectMetadataToMongoDB failure", zap.String("date", date), zap.Error(err))
-			return
-		}
-
-		total, stock, day, week, err := service.PushMetadataToRepository(Source, date)
-		if err != nil {
-			zlog.Error("PushMetadataToRepository failure", zap.String("date", date), zap.Error(err))
-			return
-		}
-
-		if err := service.PushTaskInfoToRepository(date, total, stock, day, week); err != nil {
-			zlog.Error("PushTaskInfoToRepository failure", zap.String("date", date), zap.Error(err))
-			return
-		}
-		ok = true
-		zlog.Info("Fetch metadata complete", zap.String("date", date), zap.Int64("fetch-count", fetchCount), zap.Int64("push-count", total), zap.Duration("cost", time.Since(now)))
-
-	})
+	_, err := c.AddFunc(DefaultCronSpec, do)
 	if err != nil {
 		zlog.Fatal("Cron add func failure", zap.Error(err))
 	}
 	c.Start()
+}
+
+func do() {
+	var (
+		now = time.Now()
+		ok  = false
+	)
+	defer func() {
+		if !ok {
+			client, close, err := client.NewClientForNotification()
+			if err != nil {
+				zlog.Error("NewClientForNotification failure", zap.Error(err))
+				return
+			}
+			defer close()
+
+			ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+			defer cancel()
+
+			if _, err := client.SendEmail(ctx, &pb.Mail{
+				To: []*pb.Contact{
+					{Name: "shepard", Address: "eviltomorrow@163.com"},
+				},
+				Subject: "[Warnning] Robber-collector Notify",
+				Body:    fmt.Sprintf("<h5>Fetch metadata failure, please check in log, date: [%v]</h5>", time.Now().Format("2006-01-02 15:04:05")),
+			}); err != nil {
+				zlog.Error("SendEmail failure", zap.Error(err))
+			}
+		}
+	}()
+
+	zlog.Info("Fetch metadata begin", zap.String("date", now.Format("2006-01-02")))
+	date, fetchCount, err := service.CollectMetadataToMongoDB(now, Source, true)
+	if err != nil {
+		zlog.Error("CollectMetadataToMongoDB failure", zap.String("date", date), zap.Error(err))
+		return
+	}
+
+	total, stock, day, week, err := service.PushMetadataToRepository(Source, date)
+	if err != nil {
+		zlog.Error("PushMetadataToRepository failure", zap.String("date", date), zap.Error(err))
+	}
+
+	if err := service.PushTaskInfoToRepository(date, total, stock, day, week); err != nil {
+		zlog.Error("PushTaskInfoToRepository failure", zap.String("date", date), zap.Error(err))
+	}
+	ok = true
+	zlog.Info("Fetch metadata complete", zap.String("date", date), zap.Int64("fetch-count", fetchCount), zap.Int64("push-count", total), zap.Duration("cost", time.Since(now)))
 }
